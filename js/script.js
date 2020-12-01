607966d9d3ac3092ccb3076f4c5d758fd8d48918
enrico <rombaldonienrico@gmail.com>
2020-12-01 16:05:39 +0100

    js5

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+//Si scriva una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
+// JS 5
+var array = [5,7,3,88,33,44];
+var max = prompt("inserisci un numero fino da 0 a"+array.length);
+var min = prompt("inserisci un numero fino da 0 a"+ (array.length-1));
+console.log(threeArgoments(array,min,max));
+
+
+
+function threeArgoments(array,max,min){
+  var newArray=[];
+  for (var i=min-1;i<max; i++){
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
