029f221981c53be5b0c541fbfd0d891ba5d34e78
enrico <rombaldonienrico@gmail.com>
2020-12-01 18:14:26 +0100

    work

--- js/script.js
+++ js/script.js
@@ -17,13 +17,13 @@
 // }
 // JS 6
 const array = [5,7,3,88,33,44];
-
 let max = prompt("inserisci un numero fino da 1 a "+array.length);
-if (controlValMinMax(array,max)) max=prompt("il numero inserito deve essere maggiore di 1 e minore di"+array.length+ "il primo numero inserito :"+min+" deve essere maggiore del secondo"+ max);
 let min = prompt("inserisci un numero fino da 1 a "+ (array.length));
-if (controlValMinMax(array,min)) min=prompt("il numero inserito deve essere maggiore di 1 e minore di"+array.length+ "il primo numero inserito :"+min+" deve essere maggiore o uguale del secondo"+ max);;
+const controlValMinMax=(array,control) => (control<1 && control> array.length || max<min);
+if (controlValMinMax(array,max)) max=prompt("MAX :il numero inserito deve essere maggiore di 1 e minore di"+array.length+ "il primo numero inserito :"+min+" deve essere maggiore del secondo "+ max);
+if (controlValMinMax(array,min)) min=prompt("MIN: il numero inserito deve essere maggiore di 1 e minore di"+array.length+ "il primo numero inserito :"+ min +" deve essere maggiore o uguale del secondo "+ max);
 // if(max<1 min> array.length) min = prompt("il numero inserito deve essere maggiore di 1 e minore di"+array.length);
-const controlValMinMax=(array,control) => console.log((control<1 && control> array.length || max > min));
+
 
 
 const cutArgoments= (array,min,max) => {
